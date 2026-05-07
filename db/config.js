076b2154d8d1e1cbd5fbb2a2984794d05f285545
ydsdfg232uhu231h <yuvrajgupta90334@gmail.com>
2026-05-07 21:11:07 +0530

    new version 1.1

--- db/config.js
+++ db/config.js
@@ -1,8 +1,12 @@
 const config = {
     host: process.env.HOST,
+    port: process.env.DBPORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    },
 };
 
 module.exports = config;
