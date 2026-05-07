175913a4a745aece661f25d7c321fa655eb48584
ydsdfg232uhu231h <145591042+ydsdfg232uhu231h@users.noreply.github.com>
2026-05-07 06:52:53 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { register, login } = require('../controllers/authControllers');
 const router = express.Router();
-router.post("/api/account/sign", register);
-router.post("/api/account/login", login);
+router.post("/account/sign", register);
+router.post("/account/login", login);
 
 module.exports = router;
